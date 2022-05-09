7f3994608ed9576c4f5cbb3559aebbbff21809d5
Aron Adams <aronrapetto@hotmail.com>
2022-05-09 17:27:30 -0300

    estilizacao dos titulos

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Title.css';
 
 class Title extends React.Component {
   render() {
     const { headline } = this.props;
-    return <h2>{headline}</h2>;
+    return <h2 className="titulo">{headline}</h2>;
   }
 }
 
